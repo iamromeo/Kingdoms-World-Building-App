0d08b0745ad56314e4c385682857546c0deef84b
Dimitris Havlidis <hello@iamromeo.com>
2016-10-13 21:53:10 +0100

    Corrections on deprecation of ContainerAware

--- src/Portal/Bundle/PublicBundle/Menu/Builder.php
+++ src/Portal/Bundle/PublicBundle/Menu/Builder.php
@@ -2,18 +2,21 @@
 namespace Portal\Bundle\PublicBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
-use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
-class Builder extends ContainerAware
+class Builder implements ContainerAwareInterface
 {
-    public function mainMenu(FactoryInterface $factory, array $options)
-    {
-        $menu = $factory->createItem('root');
-        $menu->addChild('Home', array('route' => 'homepage'));
+	use ContainerAwareTrait;
 
-        // $menu->addChild('Tools')->setAttribute('dropdown',true);
-        //     $menu['Tools']->addChild('Name generator', array('route' => 'tools_namegenerator_homepage'));
-        //     $menu['Tools']->addChild('Notes', array('route' => 'note'));
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {    	
+        $menu = $factory->createItem('root');
+        $menu->addChild('home', array('route' => 'homepage'));
+
+        $menu->addChild('tools')->setAttribute('dropdown',true);
+            $menu['tools']->addChild('name_generator', array('route' => 'tools_namegenerator_homepage'));
+            // $menu['Tools']->addChild('Notes', array('route' => 'note'));
         return $menu;
     }
 }
